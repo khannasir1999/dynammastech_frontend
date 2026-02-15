6ad71579ad5ab599b9949bce0231cce5981e46f7
khannasir1999 <mn41529@gmail.com>
2026-02-15 15:20:41 +0500

    tweak

--- src/pages/tasks.jsx
+++ src/pages/tasks.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import "../../../frontend/src/task.css";
+import "../task.css";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api";
 
